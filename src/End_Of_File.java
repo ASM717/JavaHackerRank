d127d6b5a979cf3c830903ec993ef4972f7faa89
ASM717 <stepan151@gmail.com>
2020-08-13 08:44:29 +0300

    homework

---
+++ src/End_Of_File.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class End_Of_File {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        for(int i = 1; scan.hasNext()== true; i++) {
+            System.out.println(i + " " + scan.nextLine());
+        }
+    }
+}
