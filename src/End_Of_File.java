39938df6b7f3369dbf6a5aa64fd96aafeb5da97c
ASM717 <stepan151@gmail.com>
2021-02-07 20:04:15 +0300

    homework

--- src/End_Of_File.java
+++ src/End_Of_File.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class End_Of_File {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        for(int i = 1; scan.hasNext()== true; i++) {
+        for(int i = 1; scan.hasNext(); i++) {
             System.out.println(i + " " + scan.nextLine());
         }
     }
